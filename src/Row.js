82b3a3cf22e6a7c444d2fae27e79640619b1cede
Amirmasoud Garavand <garavandamirmasoud@gmail.com>
2022-03-12 11:56:38 -0800

    first commit

---
+++ src/Row.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(
+        `https://api.themoviedb.org/3${fetchUrl}`
+      );
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  async function handleClick(movie) {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      await movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch(() => alert("sorry not trailer"));
+    }
+  }
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      //https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="row">
+      <h2>{title} </h2>
+      {/* title */}
+      <div className="row__posters">
+        {/* container -> row poster */}
+        {movies.map((movie) => (
+          <img
+            onClick={() => handleClick(movie)}
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
